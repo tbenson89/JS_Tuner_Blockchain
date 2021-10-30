6dffe9dcc9cb9a2e57e2102dba7f69830585dadc
A. Tyler Benson <typrobenson@gmail.com>
2021-10-30 11:46:54 -0600

    A fun little JS blockchain

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Block, Blockchain, TunerChain } = require("./tunerChain.js");
+
+/* Features of TunerChain */
+// Blockchain is the blockchain class, which means you can inherit this class and upgrade TunerChain if you want.
+// TunerChain is a "Blockchain" object, which is ready to use.
+// TunerChain.chain // The whole chain
+// TunerChain.difficulty // The difficulty
+// TunerChain.getLastBlock() // The latest block
+// TunerChain.isValid() // "true" if chain is valid, "false" otherwise.
+// new Block(timestamp /*string - "optional"*/, data /*array - "optional"*/) // Creates a new "Block" object.
+// TunerChain.addBlock(block) // Mines the block and add the block to the chain.
+
+console.log("Deploying...");
+
+// A transaction example:
+TunerChain.addBlock(new Block(Date.now().toString(), [{ from: "John", to: "Bob", amount: 100 }]));
+TunerChain.addBlock(new Block(Date.now().toString(), [{ from: "nguyenphuminh", to: "girlfriend", amount: 100 }]));
+
+
+console.log(TunerChain.chain);
---
+++ tunerChain.js
@@ -0,0 +1,74 @@
+// Create Has Function
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+
+// Create A Block
+class Block {
+
+    constructor(timestamp = "", data = []) {
+        this.timestamp = timestamp;
+        // Data -> contains info like transaction = send/receive, smart-contracts, etc
+        this.data = data;
+        this.hash = this.getHash();
+        this.prevHash = ""; // Previous Parent block's hash
+        this.nonce = 0;
+    }
+
+    getHash() { return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data)); }
+
+    mine(difficulty) {
+        console.log("Mining has begun, process can take a long time!");
+        // Basically, loops until the substring of the hash with length of 0 is equal to the string 0...
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            // We increases our nonce so that we can get a whole different hash.
+            this.nonce++;
+            console.log(this.hash);
+            console.log(this.getHash());
+            console.log("_________________________");
+            console.log(this.hash.substring(0, difficulty) - Array(difficulty + 1).join("0"));
+            // Update our new hash with the new nonce value.
+            this.hash = this.getHash();
+        }
+    }
+}
+
+// Create the TunerChain
+class Blockchain {
+
+    constructor() {
+        // Create our genesis block (Parent)
+        this.chain = [new Block(Date.now().toString())];
+        this.difficulty = 1; // based on how many blocks mined!
+    }
+
+    getLastBlock() { return this.chain[ this.chain.length - 1 ]; }
+
+    addBlock(block) {
+        // Since we are adding a new block, prevHas (parent) will be the hash of old latest block
+        block.prevHash = this.getLastBlock().hash;
+        // Since now prevHash has a value, we must reset the block's hash
+        block.hash = block.getHash();
+        block.mine(this.difficulty);
+        this.chain.push(block);
+    }
+
+    isValid() {
+        // Iterate over the chain - every child -> parent -> genesis need to be validated everytime a block is updated or created!
+        for (let i = 0; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const prevBlock = this.chain[ i - 1 ];
+
+            // Check Validation
+            if (currentBlock.hash !== currentBlock.getHash() || prevBlock.hash !== currentBlock.prevHash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+const TunerChain = new Blockchain();
+
+module.exports.Block = Block;
+module.exports.Blockchain = Blockchain;
+module.exports.TunerChain = TunerChain;
